b2a4b24650aa1b3825219ae6d892966d4a4daa94
Tiffany Chong <sciffany@gmail.com>
2022-05-23 03:42:14 +0800

    Change about a bit

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const Constants = {
   endGameMessage:
     "\n\nCongrats to all the winners!\n\nClick on /menu to view the set of questions from your last search. Or /search new keywords",
   about:
-    "🪐 Greetings NeatNerds! 🪐\n\nNeatQuiz is a multiplayer trivia and puzzle game. Players earn points by giving the first accurate response to questions given. At the end of the game, all players win because the most valuable treasure of all is the learning and friendship gained along the way. I'm sorry, too cheesy?\n\nMore cheese 🧀\nDecks are crafted with love by creators with a wide range of interests. If you want your decks featured in trivia nights around the world, we heartily welcome your wit and creativity here on https://neatquiz.netlify.app/",
+    "🪐 Greetings NeatNerds! 🪐\n\nNeatQuiz is a multiplayer trivia and puzzle game. Players earn points by answering correctly first. When the game ends, all players win because the greatest treasure is the learning and friendship we gained along the way. I'm sorry, too cheesy?\n\nMore cheese 🧀\nDecks are crafted with love by creators with a wide range of interests. If you want your decks featured in trivia nights around the world, we heartily welcome your wit and creativity here on https://neatquiz.netlify.app/",
 };
